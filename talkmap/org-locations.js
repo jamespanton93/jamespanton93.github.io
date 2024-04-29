999ba8aebf3e9f697edeacfac1b5b2b5614261ea
James Panton <pantonjc@cardiff.ac.uk>
2024-04-29 22:37:12 +0100

    update talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,40 @@
 var addressPoints = [
   [
+    "Leeds, UK",
+    53.809557,
+    -1.554580
+  ],
+  [
+    "Norwich, UK (remote)",
+    52.621438,
+    1.238206
+  ],
+  [
+    "Toronto, Canada (remote)",
+    43.660023,
+    -79.399287
+  ],
+  [
+    "Leicester, UK",
+    52.621055,
+    -1.124708
+  ],
+  [
+    "Liverpool, UK",
+    53.407917,
+    -2.968692
+  ],
+  [
+    "Vienna, Austria",
+    48.234880,
+    16.414100
+  ],
+  [
+    "Hévíz, Hungary",
+    46.786732,
+    17.186846
+  ],
+  [
     "San Francisco, California",
     37.7790262,
     -122.4199061
